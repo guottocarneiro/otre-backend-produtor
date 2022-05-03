8c1bc4bf69118a37991eedba68bb3a3aa52b29d0
Gustavo Otto Carneiro <guotto.carneiro@gmail.com>
2022-05-03 10:46:35 -0300

    Primeiro commit

---
+++ src/main/java/com/pucminas/otre/otreprodutor/OtreProdutorApplication.java
@@ -0,0 +1,13 @@
+package com.pucminas.otre.otreprodutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtreProdutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtreProdutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pucminas/otre/otreprodutor/OtreProdutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucminas.otre.otreprodutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtreProdutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
